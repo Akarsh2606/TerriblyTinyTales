0eab0f68199efbc8157d445b46e084d166907d4b
Akarsh2606 <115339906+Akarsh2606@users.noreply.github.com>
2023-05-15 02:25:33 +0530

    Initial commit

--- src/ExportButton.js
+++ src/ExportButton.js
@@ -4,13 +4,10 @@ import Papa from 'papaparse';
 
 const ExportButton = ({ histogramData }) => {
   const handleExport = () => {
-    // Convert histogram data to CSV format using PapaParse library
     const csvData = Papa.unparse(histogramData);
 
-    // Create a new Blob with the CSV data
     const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8' });
 
-    // Use the file-saver library to save the Blob as a CSV file
     saveAs(blob, 'histogram_data.csv');
   };
 
